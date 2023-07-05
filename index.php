99936b155e9594e297dc8e40e74ec581aba7f771
niccoloruberti <nikiruberti@gmail.com>
2023-07-05 18:22:59 +0200

    Milestone 2 - completata

--- index.php
+++ index.php
@@ -3,33 +3,7 @@
 var_dump($_GET['pswLength']);
 var_dump(!empty($_GET['paswlength']));
 
-//funzione per generare la password
-function generatePassword($pswLength) {
-
-    //creo gli array da cui prendere gli elemnti per creare la password
-    $uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    $lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    $numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    $symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
-
-    //mescolo insieme i caratteri
-    $characters = array_merge($uppercase, $lowercase, $numbers, $symbols);
-    var_dump($characters);
-    $charactersShuffled = $characters;
-    shuffle($charactersShuffled);
-    var_dump($charactersShuffled);
-
-
-    //generazione della password
-    $password = '';
-    for ($i = 1; $i <= $pswLength; $i++) {
-        $randomIndex = rand(0, count($characters) - 1);
-        var_dump($randomIndex);
-        $password .= $characters[$randomIndex];
-    }
-    var_dump($password);
-    return $password;
-}
+include 'functions.php';
 
 if (isset($_GET['pswLength']) && $_GET['pswLength'] !== 0 && !empty($_GET['pswLength'])) {
     $password = generatePassword($_GET['pswLength']);
